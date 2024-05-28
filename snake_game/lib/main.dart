8cd3988b52edd495743c04f1dced6bbe22fe469d
DELILLE Elone <elone.delille@algosup.com>
2024-05-28 14:44:15 +0200

    GitHub Commit
    Commit of the github

--- snake_game/lib/main.dart
+++ snake_game/lib/main.dart
@@ -9,12 +9,132 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    var size = MediaQuery.sizeOf(context);
+    generateRow() {
+      List<Widget> rowList = [];
+      for (int i = 0; i < 10; i++) {
+        rowList.add(containerRow(context, i));
+      }
+      return rowList;
+    }
+
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+        appBar: AppBar(
+          title: const Text(
+            "Algogo",
+            style: TextStyle(
+              color: Colors.black,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          centerTitle: true,
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Column(children: [...generateRow()]),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  // up button
+                  children: [
+                    GestureDetector(
+                      onTap: () {
+                        debugPrint("Up button tapped");
+                      },
+                      child: Icon(
+                        Icons.keyboard_arrow_up,
+                        size: size.width * 0.09,
+                      ),
+                    ),
+                  ],
+                )
+              ],
+            ),
+            Row(
+              // left and right button
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                GestureDetector(
+                  child: Icon(
+                    Icons.keyboard_arrow_left,
+                    size: size.width * 0.09,
+                  ),
+                  onTap: () {
+                    debugPrint("Left Button");
+                  },
+                ),
+                GestureDetector(
+                  child: Icon(
+                    Icons.keyboard_arrow_right,
+                    size: size.width * 0.09,
+                  ),
+                  onTap: () {
+                    debugPrint("Right Button");
+                  },
+                ),
+              ],
+            ),
+            Row(
+              // down button
+              children: [
+                GestureDetector(
+                  child:
+                      Icon(Icons.keyboard_arrow_down, size: size.width * 0.09),
+                  onTap: () {
+                    debugPrint("Down Button");
+                  },
+                ),
+              ],
+            ),
+          ],
         ),
       ),
     );
   }
+}
+
+Widget containerRow(BuildContext context, int index_X) {
+  generateColumn() {
+    List<Widget> columnList = [];
+    for (int i = 0; i < 10; i++) {
+      columnList.add(columnCase(context, index_X, i));
+    }
+    return columnList;
+  }
+
+  return Container(
+    child: Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: generateColumn(),
+    ),
+  );
+}
+
+Widget columnCase(BuildContext context, int iX, int iY) {
+  var size = MediaQuery.sizeOf(context);
+  return Column(
+    children: [
+      Container(
+        width: size.width * 0.09,
+        height: size.width * 0.09,
+        decoration: BoxDecoration(
+          color: Colors.red,
+          border: Border(
+              left: const BorderSide(color: Colors.black),
+              right: (iY == 9)
+                  ? const BorderSide(color: Colors.black)
+                  : BorderSide.none,
+              bottom: (iX == 9)
+                  ? const BorderSide(color: Colors.black)
+                  : BorderSide.none,
+              top: const BorderSide(color: Colors.black)),
+        ),
+      ),
+    ],
+  );
 }
